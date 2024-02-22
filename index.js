2fd46db5828b642bec2f2b90b6c4098e3de5dc25
Anatoliy <anatoliypuss5@gmail.com>
2024-02-22 21:18:18 +0300

    Undo timeout

--- index.js
+++ index.js
@@ -4,16 +4,11 @@ const { init, close } = require("./db/db");
 const routes = require("./routes/routes");
 const cors = require("cors");
 const { config } = require("./config");
-const timeout = require("connect-timeout");
 
 const app = express();
-app.use(timeout(config.server.timeout));
 app.use(cors());
-app.use(haltOnTimedout)
 app.use(bodyParser.json({}));
-app.use(haltOnTimedout)
 app.use(routes);
-app.use(haltOnTimedout)
 
 init().then(() => {
     console.log(`Starting server on port ${config.server.port}...`);
