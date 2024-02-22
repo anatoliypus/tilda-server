579d371ec762083426e177792bcaec08d146b81b
Anatoliy <anatoliypuss5@gmail.com>
2024-02-22 21:04:36 +0300

    Fixed timeouts

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const { init, close } = require("./db/db");
 const routes = require("./routes/routes");
 const cors = require("cors");
 const { config } = require("./config");
-const timeout = require('connect-timeout')
+const timeout = require("connect-timeout");
 
 const app = express();
-app.use(timeout(config.server.timeout))
+app.use(timeout(config.server.timeout));
 app.use(cors());
+app.use(haltOnTimedout)
 app.use(bodyParser.json({}));
+app.use(haltOnTimedout)
 app.use(routes);
+app.use(haltOnTimedout)
 
 init().then(() => {
     console.log(`Starting server on port ${config.server.port}...`);
     app.listen(config.server.port);
 });
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
