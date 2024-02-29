0c28afb5eac5856ff964bc883908868209dd5eba
Anatoliy <anatoliypuss5@gmail.com>
2024-02-29 18:06:33 +0300

    fix

--- db/db.js
+++ db/db.js
@@ -165,7 +165,7 @@ const baseGetProducts = async (
     if (resultCategoryId) {
         const childs = await getChildCategories(resultCategoryId);
         console.log(childs)
-        const ids = childs.forEach((v) => v.id);
+        const ids = childs.map((v) => v.id);
         console.log(ids)
         matchParameter.$and.push({
             categoryId: {$in: ids}
