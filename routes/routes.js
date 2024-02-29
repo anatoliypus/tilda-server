513c7a8d4fd22a9e5d67f6f375dffde2dd550f7d
Anatoliy <anatoliypuss5@gmail.com>
2024-03-01 00:35:11 +0300

    Rates

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { toPositiveInt } = require("../utils/utils");
-const { catalogHandler, itemHandler, searchHandler, searchPoizonHandler, brandsHandler } = require("./handlers");
+const {
+    catalogHandler,
+    itemHandler,
+    searchHandler,
+    searchPoizonHandler,
+    brandsHandler,
+} = require("./handlers");
 const { config } = require("../config");
-const boolean = require("@hapi/joi/lib/types/boolean");
+const fs = require("node:fs");
 
 const router = express.Router();
 
@@ -15,80 +21,136 @@ const generateResponse = (error = false, msg = "ok", body = {}) => ({
 router.get("/catalog", async (req, res) => {
     let page = toPositiveInt(req.query.page) || 1;
     let pageSize = toPositiveInt(req.query.pageSize) || config.defaultPageSize;
-    let gender = (req.query.gender && Object.values(config.genders.client).includes(req.query.gender) && req.query.gender) || config.genders.client.all
-    let category = req.query.category || null
-    let sort = req.query.sort
-    let brand = req.query.brand || null
+    let gender =
+        (req.query.gender &&
+            Object.values(config.genders.client).includes(req.query.gender) &&
+            req.query.gender) ||
+        config.genders.client.all;
+    let category = req.query.category || null;
+    let sort = req.query.sort;
+    let brand = req.query.brand || null;
 
-    if (sort != 'popularity') sort = null
+    if (sort != "popularity") sort = null;
 
     res.status(200).json(
-        generateResponse(false, "ok", await catalogHandler(page, pageSize, gender, category, sort, brand))
+        generateResponse(
+            false,
+            "ok",
+            await catalogHandler(page, pageSize, gender, category, sort, brand)
+        )
     );
 });
 
 router.get("/product", async (req, res) => {
     let id = toPositiveInt(req.query.id);
-    let cache = req.query.cache
-    if (cache == '0') cache = false
-    else cache = true
+    let cache = req.query.cache;
+    if (cache == "0") cache = false;
+    else cache = true;
     if (!id)
         return res
             .status(400)
             .json(generateResponse(true, "Please specify right id format."));
-    res.status(200).json(generateResponse(false, "ok", await itemHandler(id, cache)));
+    res.status(200).json(
+        generateResponse(false, "ok", await itemHandler(id, cache))
+    );
 });
 
 router.get("/searchPoizon", async (req, res) => {
     let key = req.query.key;
-    let page = 0
-    let pageSize = 5
+    let page = 0;
+    let pageSize = 5;
 
     if (!key)
         return res
             .status(400)
             .json(generateResponse(true, "Please specify right search key."));
     res.status(200).json(
-        generateResponse(false, "ok", await searchPoizonHandler(key, page, pageSize))
+        generateResponse(
+            false,
+            "ok",
+            await searchPoizonHandler(key, page, pageSize)
+        )
     );
-})
+});
 
 router.get("/search", async (req, res) => {
     let key = req.query.key;
     let page = toPositiveInt(req.query.page) || 1;
     let pageSize = toPositiveInt(req.query.pageSize) || config.defaultPageSize;
-    let gender = (req.query.gender && req.query.gender in Object.values(config.genders.client) && req.query.gender) || config.genders.all
-    let category = req.query.category || null
-    let sort = req.query.sort
-    let brand = req.query.brand || null
+    let gender =
+        (req.query.gender &&
+            req.query.gender in Object.values(config.genders.client) &&
+            req.query.gender) ||
+        config.genders.all;
+    let category = req.query.category || null;
+    let sort = req.query.sort;
+    let brand = req.query.brand || null;
 
     if (!key)
         return res
             .status(400)
             .json(generateResponse(true, "Please specify right search key."));
     res.status(200).json(
-        generateResponse(false, "ok", await searchHandler(key, page, pageSize, gender, category, sort, brand))
+        generateResponse(
+            false,
+            "ok",
+            await searchHandler(
+                key,
+                page,
+                pageSize,
+                gender,
+                category,
+                sort,
+                brand
+            )
+        )
     );
 });
 
 router.get("/brands", async (req, res) => {
-    res.status(200).json(
-        generateResponse(false, "ok", await brandsHandler())
-    );
+    res.status(200).json(generateResponse(false, "ok", await brandsHandler()));
 });
 
+router.get("/setRate", async (req, res) => {
+    const rate = parseFloat(req.query.rate);
 
-router.get('/js', async (req, res) => {
-    res.sendFile(config.server.jsFile)
-})
+    fs.readFile("../utils/ratesToken.txt", "utf8", (err, data) => {
+        if (err) {
+            res.status(500).json(
+                generateResponse(true, "неизвестная ошибка: " + err)
+            );
+        } else {
+            const token = req.query.token == data;
 
-router.get('/css', async (req, res) => {
-    res.sendFile(config.server.cssFile)
-})
+            if (!token)
+                res.status(403).json(generateResponse(true, "неверный токен"));
 
-router.get('/demoPrices', async (req, res) => {
-    res.sendFile(config.server.demoHTML)
-})
+            const content = JSON.stringify({ rate });
+
+            fs.writeFile("../utils/rates.json", content, (err) => {
+                if (err) {
+                    res.status(500).json(
+                        generateResponse(true, "неизвестная ошибка: " + err)
+                    );
+                } else {
+                    res.status(200).json(generateResponse(false, "ok"));
+                }
+            });
+        }
+    });
+});
+
+router.get("/js", async (req, res) => {
+    res.sendFile(config.server.jsFile);
+});
+
+router.get("/css", async (req, res) => {
+    res.sendFile(config.server.cssFile);
+});
+
+router.get("/demoPrices", async (req, res) => {
+    res.sendFile(config.server.demoHTML);
+});
 
 router.all("*", function (req, res) {
     res.status(404).json(generateResponse(true, "Unknown route"));
